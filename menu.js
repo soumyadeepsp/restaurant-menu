260004e0078d1a159fb88afbd49211edb5b16aa8
soupaul_expedia <soupaul@expediagroup.com>
2025-03-09 12:52:50 +0530

    added menu list

---
+++ menu.js
@@ -0,0 +1,79 @@
+console.log(menuItemsData);
+
+const menuButtons = document.querySelectorAll('.menu-search-categories button');
+
+for (let i=0; i<menuButtons.length; i++) {
+  menuButtons[i].addEventListener('click', function() {
+    for (let j=0; j<menuButtons.length; j++) {
+      menuButtons[j].classList.remove('active');
+    }
+    menuButtons[i].classList.add('active');
+  });
+}
+
+const menuItems = document.querySelectorAll('.menu-item');
+console.log(menuItems);
+for (let i=0; i<menuItems.length; i++) {
+  menuItems[i].addEventListener('onmouseover', function() {
+    console.log(menuItems[i]);
+  });
+}
+
+const menuItemsContainer = document.querySelector('.menu-items');
+for (let i=0; i<menuItemsData.length; i++) {
+  menuItemsContainer.innerHTML = menuItemsContainer.innerHTML + `<div class="menu-item">
+      <img src=${menuItemsData[i].image} alt="menu1" class="menu-item-image">
+      <div class="menu-item-details">
+        <h1>${menuItemsData[i].title}</h1>
+        <span>${menuItemsData[i].description}</span>
+        <span class="price">Price: Rs. ${menuItemsData[i].price}</span>
+        <button id=${menuItemsData[i].id} class="add-to-cart hidden">Add to Cart</button>
+      </div>
+    </div>`;
+  // let menuItem = document.createElement('div');
+  // menuItem.classList.add('menu-item');
+  // menuItemsContainer.appendChild(menuItem);
+  // let image = document.createElement('img');
+  // image.src = menuItemsData[i].image;
+  // image.alt = 'menu1';
+  // image.classList.add('menu-item-image');
+  // menuItem.appendChild(image);
+  // let details = document.createElement('div');
+  // details.classList.add('menu-item-details');
+  // menuItem.appendChild(details);
+  // let title = document.createElement('h1');
+  // title.innerText = menuItemsData[i].title;
+  // details.appendChild(title);
+  // let description = document.createElement('span');
+  // description.innerText = menuItemsData[i].description;
+  // details.appendChild(description);
+  // let price = document.createElement('span');
+  // price.innerText = `Price: Rs. ${menuItemsData[i].price}`;
+  // price.classList.add('price');
+  // details.appendChild(price);
+  // let button = document.createElement('button');
+  // button.id = menuItemsData[i].id;
+  // button.classList.add('add-to-cart');
+  // button.innerText = 'Add to Cart';
+  // button.classList.add('hidden');
+  // details.appendChild(button);
+}
+
+localStorage.setItem('cart', JSON.stringify([]));
+console.log(localStorage.getItem('cart'));
+
+const addtoCartButtons = document.querySelectorAll('.add-to-cart');
+for (let i=0; i<addtoCartButtons.length; i++) {
+  addtoCartButtons[i].addEventListener('click', function() {
+    console.log(addtoCartButtons[i].id);
+    menuItemsData.forEach(menuItem => {
+      if (menuItem.id == addtoCartButtons[i].id) {
+        console.log(menuItem);
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        cart.push(menuItem);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        console.log(localStorage.getItem('cart'));
+      }
+    });
+  });
+}
---
+++ menulist.js
@@ -0,0 +1,43 @@
+const menuItemsData = [{
+    "id": 1,
+    "image": "best-seller-1.jpg",
+    "title": "Paneer Butter Masala",
+    "description": "Paneer cooked in a rich tomato gravy with butter and cream",
+    "price": 250
+}, {
+    "id": 2,
+    "image": "best-seller-2.jpg",
+    "title": "Chicken Biryani",
+    "description": "Basmati rice cooked with chicken, spices and herbs",
+    "price": 300
+}, {
+    "id": 3,
+    "image": "best-seller-3.jpg",
+    "title": "Veg Manchurian",
+    "description": "Vegetable balls cooked in a spicy and tangy sauce",
+    "price": 200
+}, {
+    "id": 4,
+    "image": "best-seller-1.jpg",
+    "description": "Crispy fried chicken tossed in a spicy sauce",
+    "title": "Chilli Chicken",
+    "price": 300
+}, {
+    "id": 5,
+    "image": "best-seller-2.jpg",
+    "title": "Paneer Tikka",
+    "description": "Paneer marinated in spices and grilled",
+    "price": 250
+}, {
+    "id": 6,
+    "image": "best-seller-3.jpg",
+    "title": "Chicken Tikka",
+    "description": "Chicken marinated in spices and grilled",
+    "price": 300
+}, {
+    "id": 7,
+    "image": "best-seller-1.jpg",
+    "title": "Veg Fried Rice",
+    "description": "Rice cooked with vegetables and spices",
+    "price": 200
+}]
